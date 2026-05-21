d20e834517eddc4d18e5d833f452bc46a7c18c01
jna2158 <jna215843@gmail.com>
2026-05-21 20:30:38 +0900

    Add Clarity local opt-out

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -133,6 +133,10 @@ export default function RootLayout({
           dangerouslySetInnerHTML={{
             __html: `
               (function(c,l,a,r,i,t,y){
+                try {
+                  if (l.defaultView.localStorage.getItem("clarityOptOut") === "true") return;
+                } catch (e) {}
+
                 c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
                 t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
                 y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
